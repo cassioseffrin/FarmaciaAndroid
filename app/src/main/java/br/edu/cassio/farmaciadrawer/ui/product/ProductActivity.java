0124c468675643a40a8c22a3007e463479d8dc4d
Cassio Seffrin <cassioseffrin@gmail.com>
2021-10-27 00:35:29 -0300

    drawer completo

---
+++ app/src/main/java/br/edu/cassio/farmaciadrawer/ui/product/ProductActivity.java
@@ -0,0 +1,16 @@
+package br.edu.cassio.farmaciadrawer.ui.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.cassio.farmaciadrawer.R;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
